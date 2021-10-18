c33922aa171db99d3427e84e95fab926143df1ed
Siang Jo Yee <joyeesiang@gmail.com>
2021-10-18 16:39:17 +0800

    Added updated files

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -3,8 +3,8 @@ vegaEmbed("#choropleth_map", vg_1)
   .then(function (result) {})
   .catch(console.error);
 
-var vg_2 = "js/area_chart.vg.json";
-vegaEmbed("#area_chart", vg_2)
+var vg_2 = "js/line_chart.vg.json";
+vegaEmbed("#line_chart", vg_2)
   .then(function (result) {})
   .catch(console.error);
 
@@ -13,12 +13,17 @@ vegaEmbed("#bar_chart", vg_3)
   .then(function (result) {})
   .catch(console.error);
 
-var vg_4 = "js/parallel_coord.vg.json";
-vegaEmbed("#parallel_chart", vg_4)
+var vg_4 = "js/health.vg.json";
+vegaEmbed("#scatterplot_health", vg_4)
   .then(function (result) {})
   .catch(console.error);
 
-var vg_5 = "js/rank_line_chart.vg.json";
-vegaEmbed("#line_chart", vg_5)
+var vg_5 = "js/gdp.vg.json";
+vegaEmbed("#scatterplot_gdp", vg_5)
+  .then(function (result) {})
+  .catch(console.error);
+
+var vg_6 = "js/rank.vg.json";
+vegaEmbed("#rank_chart", vg_6)
   .then(function (result) {})
   .catch(console.error);
